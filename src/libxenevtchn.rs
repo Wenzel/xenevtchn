34063a632da49795cc6ea4c8fb46841c5fca6085
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-12-20 14:41:21 +0100

    libxenevtchn: add xenevtchn_close

--- src/libxenevtchn.rs
+++ src/libxenevtchn.rs
@@ -23,6 +23,9 @@ type FnXenevtchnFd = fn(xce: *mut xenevtchn_handle) -> c_int;
 type FnXenevtchnOpen =
     fn(logger: *mut xentoollog_logger, open_flags: ::std::os::raw::c_uint) -> *mut xenevtchn_handle;
 
+// xenevtchn_close
+type FnXenevtchnClose = fn(xce: *mut xenevtchn_handle) -> c_int;
+
 //xenevtchn_bind_interdomain
 type FnXenevtchnBindInterdomain = fn(
     xce: *mut xenevtchn_handle,
@@ -41,6 +44,7 @@ pub struct LibXenEvtchn {
     pub xenevtchn_notify: RawSymbol<FnXenevtchnNotify>,
     pub xenevtchn_fd: RawSymbol<FnXenevtchnFd>,
     pub xenevtchn_open: RawSymbol<FnXenevtchnOpen>,
+    pub xenevtchn_close: RawSymbol<FnXenevtchnClose>,
     pub xenevtchn_bind_interdomain: RawSymbol<FnXenevtchnBindInterdomain>,
     pub xenevtchn_unbind: RawSymbol<FnXenevtchnUnbind>,
 }
@@ -68,6 +72,9 @@ impl LibXenEvtchn {
         let xenevtchn_open_sym: Symbol<FnXenevtchnOpen> = lib.get(b"xenevtchn_open\0").unwrap();
         let xenevtchn_open = xenevtchn_open_sym.into_raw();
 
+        let xenevtchn_close_sym: Symbol<FnXenevtchnClose> = lib.get(b"xenevtchn_close\0").unwrap();
+        let xenevtchn_close = xenevtchn_close_sym.into_raw();
+
         let xenevtchn_bind_interdomain_sym: Symbol<FnXenevtchnBindInterdomain> =
             lib.get(b"xenevtchn_bind_interdomain\0").unwrap();
         let xenevtchn_bind_interdomain = xenevtchn_bind_interdomain_sym.into_raw();
@@ -83,6 +90,7 @@ impl LibXenEvtchn {
             xenevtchn_notify,
             xenevtchn_fd,
             xenevtchn_open,
+            xenevtchn_close,
             xenevtchn_bind_interdomain,
             xenevtchn_unbind,
         }
