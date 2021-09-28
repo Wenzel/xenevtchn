75f73daf25d709bcaa47e12d0454f9010fd074a4
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-09-29 01:28:35 +0200

    remove unwraps when loading the library

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ extern crate xenevtchn_sys;
 use libxenevtchn::LibXenEvtchn;
 use log::debug;
 use std::convert::TryInto;
-use std::io::Error;
+use std::io::Error as IoError;
+use std::error::Error;
 use std::os::raw::c_int;
 use std::ptr::null_mut;
 use xenevtchn_sys::{evtchn_port_t, xenevtchn_handle, xenevtchn_port_or_error_t};
@@ -16,17 +17,17 @@ pub struct XenEventChannel {
 }
 
 impl XenEventChannel {
-    pub fn new(domid: u32, evtchn_port: u32) -> Result<Self, Error> {
-        let libxenevtchn = unsafe { LibXenEvtchn::new() };
+    pub fn new(domid: u32, evtchn_port: u32) -> Result<Self, Box<dyn Error>> {
+        let libxenevtchn = unsafe { LibXenEvtchn::new()? };
         let handle = (libxenevtchn.xenevtchn_open)(null_mut(), 0);
         if handle.is_null() {
-            return Err(Error::last_os_error());
+            return Err(Box::new(IoError::last_os_error()));
         }
         debug!("binding interdomain on remote port {}", evtchn_port);
         let bind_port = (libxenevtchn.xenevtchn_bind_interdomain)(handle, domid, evtchn_port);
         debug!("local port = {}", bind_port);
         if bind_port < 0 {
-            return Err(Error::last_os_error());
+            return Err(Box::new(IoError::last_os_error()));
         }
 
         Ok(XenEventChannel {
@@ -36,10 +37,11 @@ impl XenEventChannel {
         })
     }
 
-    pub fn xenevtchn_pending(&self) -> Result<xenevtchn_port_or_error_t, Error> {
+    pub fn xenevtchn_pending(&self) -> Result<xenevtchn_port_or_error_t, IoError> {
+        debug!("xenevtchn_pending");
         let port = (self.libxenevtchn.xenevtchn_pending)(self.handle);
         if port < 0 {
-            return Err(Error::last_os_error());
+            return Err(IoError::last_os_error());
         }
         Ok(port)
     }
@@ -48,25 +50,25 @@ impl XenEventChannel {
         self.bind_port
     }
 
-    pub fn xenevtchn_fd(&self) -> Result<i32, Error> {
+    pub fn xenevtchn_fd(&self) -> Result<i32, IoError> {
         Ok((self.libxenevtchn.xenevtchn_fd)(self.handle))
     }
 
-    pub fn xenevtchn_unmask(&self, port: evtchn_port_t) -> Result<(), Error> {
+    pub fn xenevtchn_unmask(&self, port: evtchn_port_t) -> Result<(), IoError> {
         debug!("unmasking local port {}", port);
         let res = (self.libxenevtchn.xenevtchn_unmask)(self.handle, port);
         if res < 0 {
-            return Err(Error::last_os_error());
+            return Err(IoError::last_os_error());
         }
         Ok(())
     }
 
-    pub fn xenevtchn_notify(&self) -> Result<(), Error> {
+    pub fn xenevtchn_notify(&self) -> Result<(), IoError> {
         debug!("notifying local port {}", self.bind_port);
         let res =
             (self.libxenevtchn.xenevtchn_notify)(self.handle, self.bind_port.try_into().unwrap());
         if res < 0 {
-            return Err(Error::last_os_error());
+            return Err(IoError::last_os_error());
         }
         Ok(())
     }
--- src/libxenevtchn.rs
+++ src/libxenevtchn.rs
@@ -1,4 +1,4 @@
-use libloading::{os::unix::Symbol as RawSymbol, Library, Symbol};
+use libloading::{os::unix::Symbol as RawSymbol, Library, Symbol, Error};
 use log::info;
 use std::os::raw::c_int;
 use xenevtchn_sys::{
@@ -50,40 +50,40 @@ pub struct LibXenEvtchn {
 }
 
 impl LibXenEvtchn {
-    pub unsafe fn new() -> Self {
+    pub unsafe fn new() -> Result<Self, Error> {
         info!("Loading {}", LIBXENEVTCHN_FILENAME);
-        let lib = Library::new(LIBXENEVTCHN_FILENAME).unwrap();
+        let lib = Library::new(LIBXENEVTCHN_FILENAME)?;
 
         let xenevtchn_pending_sym: Symbol<FnXenevtchnPending> =
-            lib.get(b"xenevtchn_pending\0").unwrap();
+            lib.get(b"xenevtchn_pending\0")?;
         let xenevtchn_pending = xenevtchn_pending_sym.into_raw();
 
         let xenevtchn_unmask_sym: Symbol<FnXenevtchnUnmask> =
-            lib.get(b"xenevtchn_unmask\0").unwrap();
+            lib.get(b"xenevtchn_unmask\0")?;
         let xenevtchn_unmask = xenevtchn_unmask_sym.into_raw();
 
         let xenevtchn_notify_sym: Symbol<FnXenevtchnNotify> =
-            lib.get(b"xenevtchn_notify\0").unwrap();
+            lib.get(b"xenevtchn_notify\0")?;
         let xenevtchn_notify = xenevtchn_notify_sym.into_raw();
 
-        let xenevtchn_fd_sym: Symbol<FnXenevtchnFd> = lib.get(b"xenevtchn_fd\0").unwrap();
+        let xenevtchn_fd_sym: Symbol<FnXenevtchnFd> = lib.get(b"xenevtchn_fd\0")?;
         let xenevtchn_fd = xenevtchn_fd_sym.into_raw();
 
-        let xenevtchn_open_sym: Symbol<FnXenevtchnOpen> = lib.get(b"xenevtchn_open\0").unwrap();
+        let xenevtchn_open_sym: Symbol<FnXenevtchnOpen> = lib.get(b"xenevtchn_open\0")?;
         let xenevtchn_open = xenevtchn_open_sym.into_raw();
 
-        let xenevtchn_close_sym: Symbol<FnXenevtchnClose> = lib.get(b"xenevtchn_close\0").unwrap();
+        let xenevtchn_close_sym: Symbol<FnXenevtchnClose> = lib.get(b"xenevtchn_close\0")?;
         let xenevtchn_close = xenevtchn_close_sym.into_raw();
 
         let xenevtchn_bind_interdomain_sym: Symbol<FnXenevtchnBindInterdomain> =
-            lib.get(b"xenevtchn_bind_interdomain\0").unwrap();
+            lib.get(b"xenevtchn_bind_interdomain\0")?;
         let xenevtchn_bind_interdomain = xenevtchn_bind_interdomain_sym.into_raw();
 
         let xenevtchn_unbind_sym: Symbol<FnXenevtchnUnbind> =
-            lib.get(b"xenevtchn_unbind\0").unwrap();
+            lib.get(b"xenevtchn_unbind\0")?;
         let xenevtchn_unbind = xenevtchn_unbind_sym.into_raw();
 
-        LibXenEvtchn {
+        Ok(LibXenEvtchn {
             lib,
             xenevtchn_pending,
             xenevtchn_unmask,
@@ -93,6 +93,6 @@ impl LibXenEvtchn {
             xenevtchn_close,
             xenevtchn_bind_interdomain,
             xenevtchn_unbind,
-        }
+        })
     }
 }
